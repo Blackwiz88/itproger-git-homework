b613de6bcd076404818bcb8c7f217c733aa6c232
Kostiantyn Perepelytsia <perepelica.kos@gmail.com>
2022-11-05 13:48:03 +0200

    First commit to local rep

---
+++ src/com/samplePackage/ExampleClass.java
@@ -0,0 +1,7 @@
+package com.samplePackage;
+
+//Just a sample empty class to demonstrate working with git
+
+public class ExampleClass {
+
+}
---
+++ src/com/samplePackage/Main.java
@@ -0,0 +1,10 @@
+package com.samplePackage;
+
+//Just a sample empty Main class to demonstrate working with git
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
